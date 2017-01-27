aa1912ba1462ae3d752ad9f1356ffcf423a1fada
avellal14 <avellal14@LAPTOP-8UGIUUDM>
2017-01-26 20:02:21 -0500

    added navigator package

---
+++ src/org/usfirst/frc/team5422/robot/subsystems/navigator/Navigator.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team5422.robot.subsystems.navigator;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Navigator extends Subsystem {
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
