257dc17668439d86d45c63261cf82ec714c38d43
Michael Pavkovic <mpavkovic688@gmail.com>
2017-01-27 19:26:21 -0500

    Started DSIO

---
+++ src/org/usfirst/frc/team5422/robot/subsystems/dsio/ButtonIds.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team5422.robot.subsystems.dsio;
+
+/**
+ * Class to store button IDs in
+ * Be sure to make them public, static, and final
+ * Don't change any IDs listed here or remove any
+ * Just add them if needed
+ */
+public class ButtonIds {
+
+}
---
+++ src/org/usfirst/frc/team5422/robot/subsystems/dsio/DSIO.java
@@ -0,0 +1,8 @@
+package org.usfirst.frc.team5422.robot.subsystems.dsio;
+
+/**
+ * Created by michael on 1/27/17.
+ */
+public class DSIO {
+
+}
