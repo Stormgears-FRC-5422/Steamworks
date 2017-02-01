75c68c44b0c7d07b859d13092561b21aeb7d4da0
Darren Miller <darrenleomiller@gmail.com>
2017-01-31 21:32:46 -0500

    Added a dummy package since the build was broken without it.

---
+++ src/org/stormgears/WebDashboard/Diagnostics/Diagnostics.java
@@ -0,0 +1,12 @@
+package org.stormgears.WebDashboard.Diagnostics;
+
+// Dummy class added since the build was broken.
+public class Diagnostics{
+	Diagnostics() {
+		
+	}
+	
+	public static void log(String s) {
+		System.out.println(s);
+	}
+}
