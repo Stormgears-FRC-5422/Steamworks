128153824d9f31b4969c1cf7749aabc6f34bcc49
Michael Pavkovic <mpavkovic688@gmail.com>
2017-02-18 13:39:32 -0500

    New constructor for Pose

--- src/org/usfirst/frc/team5422/robot/subsystems/navigator/Pose.java
+++ src/org/usfirst/frc/team5422/robot/subsystems/navigator/Pose.java
@@ -1,21 +1,28 @@
 package org.usfirst.frc.team5422.robot.subsystems.navigator;
 
 public class Pose {
-	
-	public double x;
-	public double y;
-	public double v_x;
-	public double v_y;
-	
-	public void print(){
-		System.out.println("( " + this.x + ", " + this.y + ", " + this.v_x + ", " + this.v_y+ " )");
-	}
-	
-	Pose(double x, double y, double v_x, double v_y){
-		this.x = x;
-		this.y = y;
-		this.v_x = v_x;
-		this.v_y = v_y;
-	};
+
+    public double x;
+    public double y;
+    public double v_x;
+    public double v_y;
+
+    public Pose(double x, double y, double v_x, double v_y) {
+        this.x = x;
+        this.y = y;
+        this.v_x = v_x;
+        this.v_y = v_y;
+    }
+
+    public Pose(double x, double y, double theta, float v) {
+        this.x = x;
+        this.y = y;
+        this.v_x = Math.cos(theta) * v;
+        this.v_y = Math.sin(theta) * v;
+    }
+
+    public void print() {
+        System.out.println("( " + this.x + ", " + this.y + ", " + this.v_x + ", " + this.v_y + " )");
+    }
 
 }
