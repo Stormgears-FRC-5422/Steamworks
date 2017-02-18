1b0f6286de4068d29427366d18fe14917527c2d7
Michael Pavkovic <mpavkovic688@gmail.com>
2017-02-18 14:26:28 -0500

    Added FieldPositions

---
+++ src/org/usfirst/frc/team5422/robot/subsystems/navigator/FieldPositions.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team5422.robot.subsystems.navigator;
+
+/**
+ * Field Positions (final Pose objects)
+ */
+public class FieldPositions {
+
+
+    // Private constructor to prevent instantiation
+    private FieldPositions() {
+
+    }
+}
