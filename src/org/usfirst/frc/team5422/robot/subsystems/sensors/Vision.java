9fdd77bb354384d92a2b1e54c21750e3eae246b9
surenck <surenck@gmail.com>
2017-02-19 11:59:26 -0500

    Basic Vision code

--- src/org/usfirst/frc/team5422/robot/subsystems/sensors/Vision.java
+++ src/org/usfirst/frc/team5422/robot/subsystems/sensors/Vision.java
@@ -1,27 +1,52 @@
 package org.usfirst.frc.team5422.robot.subsystems.sensors;
 
 import org.usfirst.frc.team5422.robot.subsystems.RunnableNotifier;
+import org.usfirst.frc.team5422.utils.SteamworksConstants;
+
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class Vision extends RunnableNotifier{
-	public Vision(){
-		super("Vision", 0.001);
-	}
+	private DigitalOutput lights;
+	public static NetworkTable visionTable = NetworkTable.getTable("GRIP/myContoursReport");
 	
+	public Vision() {
+		super("Vision", 0.001);
+		System.out.println("Vision system constructed");
+		lights = new DigitalOutput(SteamworksConstants.RINGLIGHT_PORT);
+		lights.set(false);
+	}
+
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
 		super.run();
+				
 	}
 	
 	@Override
 	public void start() {
-		// TODO Auto-generated method stub
 		super.start();
 	}
 	
 	@Override
 	public void stop() {
-		// TODO Auto-generated method stub
 		super.stop();
+	}
+		
+	public void turnOffLights() {
+		lights.set(false);
+	}
+	
+	public void turnOnLights() {
+		lights.set(true);
+	}
+	
+	public void getVisionCoordinatesFromNetworkTable() { 
+		double [] defaultXArray = new double[0];
+		double [] defaultYArray = new double[0];
+		
+//		double [] centerX = visionTable.getNumberArray("centerX", defaultXArray);
+//		double [] centerY = visionTable.getNumberArray("centerY", defaultYArray);
+		
 	}
 }
