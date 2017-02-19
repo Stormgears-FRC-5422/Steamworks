5c7dabd2b6a61232504ce00c9892ea5d074b53a8
surenck <surenck@gmail.com>
2017-02-19 13:08:57 -0500

    updated WPI Mecanum Drive to implement initDriveModes

--- src/org/usfirst/frc/team5422/robot/subsystems/navigator/WPIMecanumDrive.java
+++ src/org/usfirst/frc/team5422/robot/subsystems/navigator/WPIMecanumDrive.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team5422.robot.subsystems.navigator;
 
 import com.ctre.CANTalon;
+import com.ctre.CANTalon.TalonControlMode;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
@@ -9,6 +10,8 @@ import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 import org.usfirst.frc.team5422.robot.Robot;
 import org.usfirst.frc.team5422.utils.RobotDriveConstants;
 import org.usfirst.frc.team5422.utils.SteamworksConstants;
+import org.usfirst.frc.team5422.utils.RobotDriveConstants.RobotDriveProfile;
+import org.usfirst.frc.team5422.utils.SteamworksConstants.RobotModes;
 
 public class WPIMecanumDrive extends Drive {
 	public RobotDrive robotDrive;
@@ -38,4 +41,44 @@ public class WPIMecanumDrive extends Drive {
 			// is set to zero.
 		robotDrive.mecanumDrive_Cartesian(joy.getX(), joy.getY(), joy.getZ(), 0);
 	}	
+	
+	public void initializeDriveMode(RobotModes robotRunMode, RobotDriveProfile driveProfile) {
+		if (robotRunMode == RobotModes.AUTONOMOUS) {
+			for(int i = 0; i < talons.length; i ++) {
+				CANTalon talon = talons[i];
+				talon.reverseOutput(true); 
+				talon.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
+				talon.configEncoderCodesPerRev(2048);
+				talon.changeControlMode(TalonControlMode.MotionProfile);
+				
+				//MOTION PROFILE PID for talons 0, 1, 3
+				talon.setP(RobotDriveConstants.CLONEBOT_MOTIONPROFILE_P);
+				talon.setI(RobotDriveConstants.CLONEBOT_MOTIONPROFILE_I); //0.0002
+				talon.setD(RobotDriveConstants.CLONEBOT_MOTIONPROFILE_D); //10.24
+				talon.setIZone(RobotDriveConstants.CLONEBOT_MOTIONPROFILE_IZONE); //1500
+				talon.setF(RobotDriveConstants.CLONEBOT_MOTIONPROFILE_F);
+			}
+						
+		} else { //RobotModes.TELEOP
+			for(int i = 0; i < talons.length; i ++) {			
+				talons[i].reverseOutput(true);
+				talons[i].changeControlMode(TalonControlMode.Speed);
+				//Velocity PID Values
+				talons[i].setPID(RobotDriveConstants.CLONEBOT_VELOCITY_P, 
+						RobotDriveConstants.CLONEBOT_VELOCITY_I, 
+						RobotDriveConstants.CLONEBOT_VELOCITY_D);
+				talons[i].setF(RobotDriveConstants.CLONEBOT_VELOCITY_F);
+				talons[i].setIZone(RobotDriveConstants.CLONEBOT_VELOCITY_IZONE);	
+				
+				//Position PID Values
+//				talons[i].setPID(SteamworksConstants.CLONEBOT_POSITION_P, 
+//								 SteamworksConstants.CLONEBOT_POSITION_I, 
+//								 SteamworksConstants.CLONEBOT_POSITION_D);
+//				talons[i].setF(SteamworksConstants.CLONEBOT_POSITION_F);
+//				talons[i].setIZone(SteamworksConstants.CLONEBOT_POSITION_IZONE);
+			}
+						
+		}
+	}
+	
 }
