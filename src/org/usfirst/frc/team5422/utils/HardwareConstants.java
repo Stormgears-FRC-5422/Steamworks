bc3f0f8c74a8f6e53f38f7fefb5cc84c1526aa4f
Suren Karavettil <surenck@gmail.com>
2017-03-30 21:53:42 -0400

    Added SQRT(2.0) to the Hardware Constants

--- src/org/usfirst/frc/team5422/utils/HardwareConstants.java
+++ src/org/usfirst/frc/team5422/utils/HardwareConstants.java
@@ -11,5 +11,5 @@ public class HardwareConstants {
 	public static final double WHEEL_RADIUS = 0.479;//meters
 	public static final double WHEEL_DIAMETER_INCHES = 6.0;
 	public static final double METERS_PER_TICK = 2 * PI * WHEEL_RADIUS / (float) ENCODER_RESOLUTION;
-	public static final double ROTATION_CALC_FACTOR = HardwareConstants.WHEEL_DIAMETER_INCHES * Math.PI;
+	public static final double ROTATION_CALC_FACTOR = HardwareConstants.WHEEL_DIAMETER_INCHES * Math.PI * Math.sqrt(2.0);
 }
